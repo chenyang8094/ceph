d3db03b993748517e22bd33d3ea36f54cb3a3b45
Mykola Golub <mgolub@mirantis.com>
2016-11-21 22:43:11 +0200

    rbd-mirror: delayed replication support
    Fixes: http://tracker.ceph.com/issues/15371
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

--- src/tools/rbd_mirror/ImageReplayer.h
+++ src/tools/rbd_mirror/ImageReplayer.h
@@ -291,6 +291,7 @@ private:
   librbd::journal::TagData m_replay_tag_data;
   librbd::journal::EventEntry m_event_entry;
   AsyncOpTracker m_event_replay_tracker;
+  Context *m_delayed_preprocess_task = nullptr;
 
   struct RemoteJournalerListener : public ::journal::JournalMetadataListener {
     ImageReplayer *replayer;
@@ -360,7 +361,8 @@ private:
   void handle_allocate_local_tag(int r);
 
   void preprocess_entry();
-  void handle_preprocess_entry(int r);
+  void handle_preprocess_entry_ready(int r);
+  void handle_preprocess_entry_safe(int r);
 
   void process_entry();
   void handle_process_entry_ready(int r);
